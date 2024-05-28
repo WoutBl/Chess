6d27dacb9c9e62a53d2331e8bfa144a6418aa907
Wout Blomme <wout.blomme@student.howest.be>
2024-05-29 00:10:05 +0200

    fix

--- src/hooks/MovePiece.ts
+++ src/hooks/MovePiece.ts
@@ -8,8 +8,8 @@ const selectedPosition = ref<{ x: number; y: number } | null>(null);
 
 
 export type vector2 = {
-  x: number
-  y: number
+  row: number
+  col: number
 }
 
 export enum Player {
@@ -20,14 +20,12 @@ white = 'white'
 
 const movePiece = (from: vector2, to: vector2, piece: Piece) => {
   const validMoves = getValidMoves(from, piece)
-  console.log(validMoves)
-  console.log(to)
-  const isValid = validMoves.some(move => move.x === to.x && move.y === to.y);
 
+  const isValid = validMoves.some(move => move.row === to.row && move.col === to.col);
   if (isValid) {
-    console.log("is valid");
-    BoardState.value[to.x][to.y] = piece;
-    BoardState.value[from.x][from.y] = null; // clear the old position
+
+    BoardState.value[to.row][to.col] = piece
+    BoardState.value[from.row][from.col] = null; // clear the old position
   } else {
     console.log("invalid move");
   }
@@ -49,36 +47,41 @@ const getValidMoves = (from: vector2, piece:Piece): vector2[] => {
  * @returns array of possible move locations
  */
 const isValidPawnMoves = (current: vector2, type: Player  ) : vector2[] =>  {
-  const direction = type === Player.black ? -1 : 1
+  const direction = type === Player.black ? 1 : -1
   const startRow = type === Player.white ? 6 : 1
   const availableMoves: vector2[] = []
-
+  console.log(type)
   // immutable
   const boardState = BoardState.value
 
-  // check if there is a piece in front of the pawn
-  const canMoveForward: boolean = !!(boardState[current.x][current.y + direction] )
-  const canMoveForwardTwice = !!(boardState[current.x][current.y + 2 * direction] )
-  const canAttackRight = (boardState[current.x + direction][current.y + direction]?.color !== type)
-// deze is broken
-  const canAttackLeft = (boardState[current.x - direction][current.y + direction]?.color !== type)
-
-
-  if(canMoveForward && startRow !== current.y){
-    console.log("canMoveForward")
-    availableMoves.push({ x: current.x, y: current.y + direction })
-  }if (canMoveForward && canMoveForwardTwice && startRow === current.y){
-    console.log("canMoveForwardtwice")
-    availableMoves.push({x: current.x, y: current.y + 2 * direction})
-  }if(canAttackLeft){
-    console.log("left")
-    availableMoves.push({x: current.x - direction, y: current.y + direction})
-  }if(canAttackRight){
-    console.log("right")
-    availableMoves.push({x: current.x + direction, y: current.y + direction})
+  // Check if the current position is within the bounds of the board (kinda redudant but just in case)
+  if (current.row < 0 || current.row >= boardState.length || current.col < 0 || current.col >= boardState[0].length) {
+    return availableMoves; // Return empty array if out of bounds
   }
 
-  // laatste rij checken
+
+  // Check if there is a piece in front of the pawn
+  if (!boardState[current.row + direction] || boardState[current.row + direction][current.col] == null) {
+    // If there's no piece in front, the pawn can move forward
+    availableMoves.push({ row: current.row + direction, col: current.col });
+
+    // If it's on the starting row and can move forward twice, add that option
+    if (current.row === startRow && !boardState[current.row + 2 * direction][current.col]) {
+      availableMoves.push({ row: current.row + 2 * direction, col: current.col });
+    }
+  }
+
+   // Check for diagonal attacks
+    if (boardState[current.row + direction]) {
+      // Check diagonal left
+      if (current.col > 0 && boardState[current.row + direction][current.col - 1] != null && boardState[current.row + direction][current.col - 1]?.color !== type) {
+        availableMoves.push({ row: current.row + direction, col: current.col - 1 });
+      }
+      // Check diagonal right
+      if (current.col < boardState[0].length - 1 && boardState[current.row + direction][current.col + 1] != null && boardState[current.row + direction][current.col + 1]?.color !== type) {
+        availableMoves.push({ row: current.row + direction, col: current.col + 1 });
+      }
+    }
   return availableMoves
 }
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,25 @@
 <template>
   <div class="board">
-    <div v-for="(row, x) in BoardState" :key="x" class="row">
-      <div
-        v-for="(cell, y) in row"
-        :key="y"
-        class="cell"
-        @click="handleCellClick(y, x)"
-      >
+    <!-- Horizontal numbering -->
+    <div class="row header">
+      <div class="cell header-cell"></div> <!-- Empty cell for the top-left corner -->
+      <div v-for="x in BoardState[0].length" :key="'header-' + x" class="cell header-cell">
+        {{ x - 1 }} /{{String.fromCharCode(97 + x - 1)}}
+      </div>
+    </div>
+
+    <div v-for="(rows, row) in BoardState" :key="row" class="row">
+      <!-- Vertical numbering -->
+      <div class="cell header-cell">
+        {{ row }}
+      </div>
+      <div v-for="(cell, col) in rows" :key="col" class="cell" @click="handleCellClick(row, col)">
         <PieceComponent class="piece" :piece="cell" />
       </div>
     </div>
   </div>
 </template>
+
 
 <script setup lang="ts">
 import { ref, watch } from 'vue'
@@ -21,26 +29,24 @@ import {useMovePiece , type vector2 } from '@/hooks/MovePiece'
 
 type fromPieceWithLocation = {
   fromPiece: Piece | null;
-  toLocation: vector2;
+  Location: vector2;
 }
 
 const { movePiece } = useMovePiece()
 const fromPiece = ref<fromPieceWithLocation | null>(null);
 
 
-const handleCellClick = (x: number, y: number) => {
+const handleCellClick = (row: number, col: number) => {
   if(fromPiece.value?.fromPiece){
-    console.log(fromPiece.value)
-    const to: vector2 = {x: x , y: y}
-
-    movePiece(fromPiece.value.toLocation, to, fromPiece.value.fromPiece)
+    const to: vector2 = {row: row , col: col}
+    movePiece(fromPiece.value.Location, to, fromPiece.value.fromPiece)
     fromPiece.value = null
 
   }else{
     // no selected piece - select piece
     fromPiece.value = {
-      fromPiece: BoardState.value[x][y],
-      toLocation: { x, y }
+      fromPiece: BoardState.value[row][col],
+      Location: { row, col }
     }
   }
 };
@@ -72,10 +78,25 @@ const handleCellClick = (x: number, y: number) => {
   width: 100%;
   height: 100%;
 }
-.row:nth-child(even) .cell:nth-child(odd) .piece{
+.row:nth-child(even) .cell:nth-child(odd) .piece {
   filter: drop-shadow(0 0 0.5rem white);
 }
-.row:nth-child(odd) .cell:nth-child(even) .piece{
+.row:nth-child(odd) .cell:nth-child(even) .piece {
   filter: drop-shadow(0 0 0.5rem white);
+}
+.header {
+  height: 12.5%;
+  width: 100%;
+  display: flex;
+  background-color: white !important;
+}
+.header-cell {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: bold;
+  border: none; /* Remove border for header cells */
+  color: black; /* Set text color to black for visibility */
+  background-color: white !important;
 }
 </style>
