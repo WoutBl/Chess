5bb629e63efed7e6615aa0cd5a213e9de374e68c
BlommeWout <wout.blomme@student.howest.be>
2024-05-28 17:29:56 +0200

    added board, added images, added moving

---
+++ src/components/PieceComponent.vue
@@ -0,0 +1,50 @@
+<template>
+  <div v-if="piece" class="piece">
+    <img :src="getPieceImage(piece)" :alt="piece.type" class="piece-image" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { PropType } from 'vue';
+
+type Piece = {
+  type: 'pawn' | 'rook' | 'knight' | 'bishop' | 'queen' | 'king';
+  color: 'white' | 'black';
+};
+
+const props = defineProps({
+  piece: {
+    type: Object as PropType<Piece | null>,
+    required: false,
+  }
+});
+
+const getPieceImage = (piece: Piece | null): string => {
+  if (!piece) return '';
+
+  const pieceImages: { [key: string]: string } = {
+    'pawn_white': 'White/Pawn.png',
+    'rook_white': 'White/Rook.png',
+    'knight_white': 'White/Knight.png',
+    'bishop_white': 'White/Bishop.png',
+    'queen_white': 'White/Queen.png',
+    'king_white': 'White/King.png',
+    'pawn_black': 'Black/Pawn.png',
+    'rook_black': 'Black/Rook.png',
+    'knight_black': 'Black/Knight.png',
+    'bishop_black': 'Black/Bishop.png',
+    'queen_black': 'Black/Queen.png',
+    'king_black': 'Black/King.png',
+  };
+
+  return pieceImages[`${piece.type}_${piece.color}`] || '';
+};
+</script>
+
+<style scoped>
+.piece-image {
+  width: 100%;
+  height: 100%;
+  filter: drop-shadow(0 0 0.5rem black);
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,112 @@
+<template>
+  <div class="board">
+    <div v-for="(row, x) in board" :key="x" class="row">
+      <div
+        v-for="(cell, y) in row"
+        :key="y"
+        class="cell"
+        @click="handleCellClick(x, y)"
+      >
+        <PieceComponent class="piece" :piece="cell" />
+      </div>
+    </div>
+  </div>
+</template>
+
 <script setup lang="ts">
+import { ref } from 'vue';
+import PieceComponent from '../components/PieceComponent.vue';
+
+type Piece = {
+  type: 'pawn' | 'rook' | 'knight' | 'bishop' | 'queen' | 'king';
+  color: 'white' | 'black';
+};
+
+type Board = (Piece | null)[][];
+
+const initialBoard: Board = [
+  [
+    { type: 'rook', color: 'black' },
+    { type: 'knight', color: 'black' },
+    { type: 'bishop', color: 'black' },
+    { type: 'queen', color: 'black' },
+    { type: 'king', color: 'black' },
+    { type: 'bishop', color: 'black' },
+    { type: 'knight', color: 'black' },
+    { type: 'rook', color: 'black' },
+  ],
+  Array(8).fill(null).map(() => ({ type: 'pawn', color: 'black' })),
+  Array(8).fill(null),
+  Array(8).fill(null),
+  Array(8).fill(null),
+  Array(8).fill(null),
+  Array(8).fill(null).map(() => ({ type: 'pawn', color: 'white' })),
+  [
+    { type: 'rook', color: 'white' },
+    { type: 'knight', color: 'white' },
+    { type: 'bishop', color: 'white' },
+    { type: 'queen', color: 'white' },
+    { type: 'king', color: 'white' },
+    { type: 'bishop', color: 'white' },
+    { type: 'knight', color: 'white' },
+    { type: 'rook', color: 'white' },
+  ]
+];
+
+const board = ref<Board>(initialBoard);
+
+const selectedPiece = ref<Piece | null>(null);
+const selectedPosition = ref<{ x: number; y: number } | null>(null);
+
+const handleCellClick = (x: number, y: number) => {
+  const cell = board.value[x][y];
+
+  if (selectedPiece.value) {
+    // Move the selected piece
+    board.value[x][y] = selectedPiece.value;
+    if (selectedPosition.value) {
+      board.value[selectedPosition.value.x][selectedPosition.value.y] = null;
+    }
+    selectedPiece.value = null;
+    selectedPosition.value = null;
+  } else if (cell) {
+    // Select a piece
+    selectedPiece.value = cell;
+    selectedPosition.value = { x, y };
+  }
+};
 </script>
 
-<template>
-  <main>
-  </main>
-</template>
+<style scoped>
+.board {
+  width: 100%;
+  height: 100vh;
+  aspect-ratio: 1/1;
+}
+.row {
+  background-color: white;
+  height: 12.5%;
+  width: 100%;
+  display: flex;
+}
+.cell {
+  height: 100%;
+  width: 12.5%;
+}
+.row:nth-child(even) .cell:nth-child(odd) {
+  background-color: black;
+}
+.row:nth-child(odd) .cell:nth-child(even) {
+  background-color: black;
+}
+.piece {
+  width: 100%;
+  height: 100%;
+}
+.row:nth-child(even) .cell:nth-child(odd) .piece{
+  filter: drop-shadow(0 0 0.5rem white);
+}
+.row:nth-child(odd) .cell:nth-child(even) .piece{
+  filter: drop-shadow(0 0 0.5rem white);
+}
+</style>
